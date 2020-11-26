8a0056267352d769c2046c0856c50091f4eaf9ee
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-26 09:56:04 -0300

    structured home page in styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
 
 const Home = () => {
     return (
-        <div>
-            Olá
-        </div>
+        <PageContainer>
+
+            <NavBar>
+                <Link>
+                    <Logo>
+                        Zumthor
+                    </Logo>
+                </Link>
+                <Link>
+                    <ButtonNav>
+                        Entrar
+                    </ButtonNav>
+                </Link>                
+            </NavBar>
+
+            <Main>
+                <Headline>
+                    Zumthor
+                </Headline>
+                <SubHeadline>
+                    Projete atmosferas e experiências arquitetônicas com o auxílio da inteligência artificial.
+                </SubHeadline>
+                <MainActions>
+                    <Link>
+                        <ButtonNav>
+                            Experimentar
+                        </ButtonNav>
+                    </Link>
+                    <Link>
+                        <ButtonNav>
+                            Saiba Mais
+                        </ButtonNav>
+                    </Link>
+                </MainActions>                
+            </Main>
+
+        </PageContainer>
     )
 }
 
