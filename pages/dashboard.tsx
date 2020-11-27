6f3ac268664f8e82aaf31c482a979a2ae5837c7a
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-27 11:30:15 -0300

    refactor: organized dashboard code

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,16 @@
 import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
-import Nav from '../src/components/Nav'
+import { useEffect } from 'react'
+
 import { getUserInfoInLocalStorage } from '../src/helpers/localStorage'
 
+import Nav from '../src/components/Nav'
 import { PageContainer } from '../src/styles/page'
 
 const Dashboard = () => {
   const router = useRouter()
-
   useEffect(() => {
     const userData = getUserInfoInLocalStorage('userData')
-    if (!userData) {      
-      router.push('/')
-    }    
+    if (!userData) { router.push('/') }
   }, [])  
 
     return (
