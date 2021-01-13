189f040c76ffd6e326c5e69e7ee49caea48ae043
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-01-13 17:08:55 -0300

    started adding cards to dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,12 +5,27 @@ import ProjectList from '../../src/components/ProjectSlider';
 
 import { PageContainer } from '../../src/styles/page';
 
+import styled from 'styled-components';
+import { Button } from '../../src/styles/button';
+
 const Dashboard = () => {
 	const { projects, references } = useGetUserInfo();
 
 	return (
 		<PageContainer>
 			<Nav isDashboard={true} />
+
+			<CardContainerStyle>
+				<CardContainerHeaderStyle>
+					<h2>Referências</h2>
+					<CardContainerButtonStyle>
+						Adicionar
+					</CardContainerButtonStyle>
+				</CardContainerHeaderStyle>
+
+				<CardContainerBodyStyle>oi</CardContainerBodyStyle>
+			</CardContainerStyle>
+
 			<ProjectList
 				link="dashboard/reference"
 				title="Referências"
@@ -28,3 +43,30 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+export const CardContainerStyle = styled.div`
+	width: 90%;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+export const CardContainerHeaderStyle = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	width: 100%;
+	margin: 25px 0;
+`;
+export const CardContainerButtonStyle = styled(Button)`
+	color: #ccc;
+	background: transparent;
+
+	font-weight: bold;
+
+	&:hover {
+		color: white;
+		background: #ccc;
+	}
+`;
+export const CardContainerBodyStyle = styled.div``;
