253eacf1bf6a10834faa3b36a5e551fa032b77f2
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-01-19 11:39:38 -0300

    fixed typing in setemail

--- src/components/InputFieldGroup/index.tsx
+++ src/components/InputFieldGroup/index.tsx
@@ -1,42 +1,39 @@
-import React from 'react'
-import { InputContainer, InputError, InputField, InputLabel } from './styles'
+import React from 'react';
+import { InputContainer, InputError, InputField, InputLabel } from './styles';
 
 interface IInputFieldGroup {
-    inputTitle: string,
-    inputId: string,
-    inputType: string,
-    state?: string | number,
-    setState?: (e: string | number) => void    
-    inputError?: string
+	inputTitle: string;
+	inputId: string;
+	inputType: string;
+	state?: string | number;
+	setState?: (e: any | string | number) => void;
+	inputError?: string;
 }
 
-const InputFieldGroup: React.FC<IInputFieldGroup> = ({ 
-    inputTitle, 
-    inputId, 
-    inputType, 
-    inputError,
-    state,
-    setState
+const InputFieldGroup: React.FC<IInputFieldGroup> = ({
+	inputTitle,
+	inputId,
+	inputType,
+	inputError,
+	state,
+	setState
 }) => {
+	function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setState(e.currentTarget.value);
+	}
 
-    function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setState(e.currentTarget.value)
-    }
+	return (
+		<InputContainer>
+			<InputLabel htmlFor={inputId}>{inputTitle}</InputLabel>
+			<InputField
+				type={inputType}
+				id={inputId}
+				value={state}
+				onChange={handleOnChange}
+			/>
+			{inputError && <InputError>{inputError}</InputError>}
+		</InputContainer>
+	);
+};
 
-    return (
-        <InputContainer>
-            <InputLabel htmlFor={inputId}>{inputTitle}</InputLabel>
-            <InputField 
-                type={inputType} 
-                id={inputId}
-                value={state}
-                onChange={handleOnChange}
-            />
-            {inputError && <InputError>{inputError}</InputError>}            
-        </InputContainer>
-    )
-}
-
-export default InputFieldGroup
-
-
+export default InputFieldGroup;
